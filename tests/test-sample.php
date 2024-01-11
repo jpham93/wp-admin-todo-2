8c0425ef448abe5c09db4fafbe8fe48c679a3a98
James Pham <jamespham93@yahoo.com>
2024-01-10 19:21:42 -0800

    Init commit

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wp_Todo_Admin_2
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
---
+++ wp-todo-admin-2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:     WP TODO Admin 2
+ * Plugin URI:      git
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          James Pham
+ * Author URI:      https://jamespham.io
+ * Text Domain:     wp-todo-admin-2
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Todo_Admin_2
+ */
+
+// Your code starts here.
+
+
+register_activation_hook();
