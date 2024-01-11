f6786204e6c9d4639d819dfa9d009e05c459c6c9
James Pham <jamespham93@yahoo.com>
2024-01-10 19:49:06 -0800

    Boilerplate pt 1

--- wp-todo-admin-2.php
+++ wp-todo-admin-2.php
@@ -1,18 +1,38 @@
 <?php
 /**
- * Plugin Name:     WP TODO Admin 2
- * Plugin URI:      git
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          James Pham
- * Author URI:      https://jamespham.io
- * Text Domain:     wp-todo-admin-2
- * Domain Path:     /languages
- * Version:         0.1.0
+ * Plugin Name:     	WP TODO Admin 2
+ * Plugin URI:      	https://github.com/jpham93/wp-admin-todo-2
+ * Description:     	A simple WP TODO plugin for the admin dashboard.
+ * Author:          	James Pham
+ * Author URI:      	https://jamespham.io
+ * Version:            	0.1.0
+ * Requires at least:	6.4.2
+ * Requires PHP: 		8.2
+ * Text Domain:     	wp-todo-admin-2
+ * Domain Path:     	/languages
  *
- * @package         Wp_Todo_Admin_2
+ * @package         	Wp_Todo_Admin_2
  */
 
-// Your code starts here.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
+// ACTIVATION
+register_activation_hook(
+	__FILE__,
+	'wta2_activate'
+);
+function wta2_activate() {
 
-register_activation_hook();
+}
+
+// DEACTIVATION
+register_deactivation_hook(
+	__FILE__,
+	'wta2_deactivate'
+);
+function wta2_deactivate() {
+
+}
+
